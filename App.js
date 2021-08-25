82b5ea361847fa92dde9e4bb0cc54f8478f72a83
Ridwan2601 <muhamadridwan6792@gmail.com>
2021-08-25 18:07:01 +0700

    Commit-----01-------React-Native-Firebase

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import React from 'react';
-import AppNavigator from './src/router';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import React from 'react';
+// import AppNavigator from './src/router';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-function App() {
+// function App() {
+//   return (
+//     <SafeAreaProvider>
+//       <AppNavigator />
+//     </SafeAreaProvider>
+//   );
+// }
+
+// export default App;
+
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Login from './src_Firebase/components/login';
+import Signup from './src_Firebase/components/signup';
+import Dashboard from './src_Firebase/components/dashboard';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <SafeAreaProvider>
-      <AppNavigator />
-    </SafeAreaProvider>
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{title: 'Signup'}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={({title: 'Login'}, {headerLeft: null})}
+      />
+      <Stack.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={({title: 'Dashboard'}, {headerLeft: null})}
+      />
+    </Stack.Navigator>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ config/Firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB0087xbUJgAxLRDlu_VIPObvcnAr0yBfU',
+  authDomain: 'clonewa-ff96b.firebaseapp.com',
+  databaseURL: 'https://clonewa-ff96b-default-rtdb.firebaseio.com',
+  projectId: 'clonewa-ff96b',
+  storageBucket: 'clonewa-ff96b.appspot.com',
+  messagingSenderId: '909283010164',
+  appId: '1:909283010164:web:502d0990e0e68461299321',
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
